39f6bd1f670acb48037892313561e89d5d933ef4
Bogdan Negura <id.bogdann@yahoo.com>
2020-09-28 21:02:36 +0100

    initial clean up

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCj3TKgNSX2Egtzh9w1g1cZOns4mMDLqUY",
+  authDomain: "clone-68140.firebaseapp.com",
+  databaseURL: "https://clone-68140.firebaseio.com",
+  projectId: "clone-68140",
+  storageBucket: "clone-68140.appspot.com",
+  messagingSenderId: "960678936727",
+  appId: "1:960678936727:web:7188447cd429a3528fa9d2",
+  measurementId: "G-1T1FB1QM10",
+};
