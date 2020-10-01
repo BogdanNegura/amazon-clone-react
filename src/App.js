2f077525300f219c499764204cf848f17eabe995
Bogdan Negura <id.bogdann@yahoo.com>
2020-10-01 21:30:25 +0100

    start the home page and create first row with product info and product rate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/header";
+import Home from "./components/home/home";
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/* Header */}
-      {/* Home */}
+      <Home />
     </div>
   );
 }
