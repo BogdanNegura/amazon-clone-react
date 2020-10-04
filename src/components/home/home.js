36beea8a51f0ac12d63bf1ec56b9a453a7cf458f
Bogdan Negura <id.bogdann@yahoo.com>
2020-10-04 16:58:40 +0100

    finish home product

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -13,21 +13,53 @@ function Home() {
         />
         <div className="home__row">
           <Product
-            title="The lean startup"
-            price={29.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            id="2893417"
+            title="The lean startup: How to Constant Innovation Creates Radically Successfull Bussiness Paperback"
+            price={11.96}
             rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
           />
-          <Product />
+          <Product
+            id="123123"
+            title="kenwood KMix Stand Mixer for Baking, stylish Kitchen Mixer with K-beater, dough Hook and whisk, 5 Litre Glass Bowl"
+            price={239.0}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+          />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            id="1231242342363"
+            title="Samsung LC 342352323 49' Curved LED Gaming Monitor"
+            price={199.0}
+            rating={5}
+            image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+          />
+          <Product
+            id="122133233123"
+            title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+            price={98.99}
+            rating={5}
+            image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+          />
+          <Product
+            id="122434133233123"
+            title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+            price={598.99}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX4385_.jpg"
+          />
         </div>
 
         <div className="home__row">
-          <Product />
+          <Product
+            id="12243413323312563"
+            title="Samsung LC492342524 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={1094.98}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX4355_.jpg"
+          />
         </div>
       </div>
     </div>
