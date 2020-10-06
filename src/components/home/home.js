f707bf377caa73d44641d89335960e4fd3d900dd
Bogdan Negura <id.bogdann@yahoo.com>
2020-10-06 20:42:26 +0100

    update basket total

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -24,7 +24,7 @@ function Home() {
             title="kenwood KMix Stand Mixer for Baking, stylish Kitchen Mixer with K-beater, dough Hook and whisk, 5 Litre Glass Bowl"
             price={239.0}
             rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/810%2BGNdkzKL._AC_SX450_.jpg"
+            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
           />
         </div>
 
--- src/components/subtotal/subtotal.js
+++ src/components/subtotal/subtotal.js
@@ -1,16 +1,20 @@
 import { ShoppingBasket } from "@material-ui/icons";
 import React from "react";
 import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "../../redux/reducer";
+import { useStateValue } from "../../redux/state.provider";
 import "./subtotal.css";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
+              Subtotal ({basket.length} items):
               <strong>{` ${value}`}</strong>
             </p>
             <small className="subtotal_gift">
@@ -19,7 +23,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"£"}
